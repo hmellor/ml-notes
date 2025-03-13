9507d4a2e5b056fd7ac1d44d90eb5fc340ed8cef
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-03-13 18:45:21 +0000

    Remove links from footer
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,12 +16,7 @@ export const sharedPageComponents: SharedLayout = {
       }
     }),
   ],
-  footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
-  }),
+  footer: Component.Footer(),
 }
 
 // components for pages that display a single page (e.g. a single note)
