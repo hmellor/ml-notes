4219a9c7953abd3ada3613d14e22b504c53f2846
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-03-13 16:08:04 +0000

    Update config
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,17 +8,17 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "ML Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "en-GB",
+    baseUrl: "hmellor.github.io/ml-notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     generateSocialImages: true,
     theme: {
       fontOrigin: "googleFonts",
