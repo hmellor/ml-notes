1531da2c52a0281e60a069f87568b2443c5e63ae
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-03-13 21:56:03 +0000

    Lowercase breadcrumb root
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -22,7 +22,7 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
+    Component.Breadcrumbs({"rootName": "home"}),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
@@ -50,7 +50,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs({rootName: "home"}), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
